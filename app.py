f8b399971402934efe019891c613e3d554fb620d
kalexander19 <58221808+kalexander19@users.noreply.github.com>
2024-03-28 12:48:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+from flask import Flask
+import socket
 
+app = Flask(__name__)
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
+
+@app.route('/')
+def hello_cloud():
+  return 'Hello Cloud!'
+  
+@app.route('/host')
+def host_info():
+  print (f"Hostname: {hostnmae}")
+  print (f"IP Address: {ip_address}")
+
+app.run(host='0.0.0.0')
