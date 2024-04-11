3e378def8eefc981ab3a2f1230ecbc3bead7ff37
anandshahh <160291052+anandshahh@users.noreply.github.com>
2024-04-11 11:11:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_cloud():
-  return 'Hello From Anand Shah ECS Container!'
+  return 'Hello From  Shah ECS Container!'
   
 @app.route('/host')
 def host_info():
